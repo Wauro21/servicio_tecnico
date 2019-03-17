20ca46c34f84e94c97d3cea47f70e544570a61b6
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2019-03-17 20:46:03 -0300

    Update wrapper.bat

--- windos10/wrapper.bat
+++ windos10/wrapper.bat
@@ -2,7 +2,8 @@
 powershell $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent());if(-not($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))){Write-Host "¡Usted no posee privilegios de administrador! Cierre esta ventana para reintentar." ;while(1)}
 ::Conseguir mejor manera de tener directorio, no asumirlo
 ECHO "Iniciando SCRIPT"
-powershell .\test.ps1
+SET address=%cd%\script.ps1
+powershell address
 ECHO "El equipo eliminara el script y cerrara sesion a continuacion"
 ::CIERRE DE SESION
 ::ELIMINACION SCRIPT
