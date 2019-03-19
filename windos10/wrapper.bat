75c0d1bed86e69b6304b772e55209b3df08ee44b
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2019-03-19 00:35:07 -0300

    Update wrapper.bat

--- windos10/wrapper.bat
+++ windos10/wrapper.bat
@@ -1,8 +1,8 @@
 ECHO OFF
 CLS
-powershell Set-ExecutionPolicy Unrestricted -Force
 :: Se comprueba si se tiene privilegios de administrador
 powershell $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent());if(-not($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))){Write-Host "¡Usted no posee privilegios de administrador! Cierre esta ventana para reintentar." ;while(1){}}
+powershell Set-ExecutionPolicy Unrestricted -Force
 ::Conseguir mejor manera de tener directorio, no asumirlo
 ECHO "Iniciando SCRIPT"
 SET address=%~dp0script.ps1
