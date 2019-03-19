28a4bcd332f14ab43d71ccc505bb86840dc8af20
Wauro21 <38472214+Wauro21@users.noreply.github.com>
2019-03-19 01:01:58 -0300

    Update wrapper.bat

--- windos10/wrapper.bat
+++ windos10/wrapper.bat
@@ -4,15 +4,18 @@ CLS
 powershell $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent());if(-not($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))){Write-Host "¡Usted no posee privilegios de administrador! Cierre esta ventana para reintentar." ;while(1){}}
 powershell Set-ExecutionPolicy Unrestricted -Force
 ::Conseguir mejor manera de tener directorio, no asumirlo
-ECHO "Iniciando SCRIPT"
+ECHO Iniciando SCRIPT
 SET address=%~dp0script.ps1
 powershell %address%
-ECHO "El equipo eliminara el script y cerrara sesion a continuacion"
+ECHO El equipo eliminara el script y cerrara sesion a continuacion
 ::RESTAURACION DE SERVICIO SCRIPTS
 powershell Set-ExecutionPolicy Restricted -Force
-ECHO "Se ha finalizado el cambio de usuario/contraseña. A continuacion se cerrara sesión y se limpiaran los archivos temporales, guarde todo el trabajo del equipo."
+ECHO Se ha finalizado el cambio de usuario/contraseña. A continuacion se cerrara sesión y se limpiaran los archivos temporales, guarde todo el trabajo del equipo.
+::ELIMINACION SCRIPT
+
 TIMEOUT /t 30
 ::CIERRE DE SESION
 SHUTDOWN /l
-::ELIMINACION SCRIPT
+::ELIMINACION BATCH
+DEL "%~f0"
 
