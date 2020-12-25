7b7cb5053622a7e58cd73f90dd95efd52d95daa1
Mauricio <maurio.aravena@sansano.usm.cl>
2020-12-25 16:51:39 -0300

    Robust password

--- windows_10/script.ps1
+++ windows_10/script.ps1
@@ -1,10 +1,6 @@
-#Test
-$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding =
-                    New-Object System.Text.UTF8Encoding
-
 #Mensajes Pre-definidos
 
-$greetMsg = "Bienvenido. ¡Este SCRIPT le ayudara a cambiar su nombre de usuario y contraseña en Windows 10! Siga las instrucciones a continuación:"
+$greetMsg = "Bienvenido. Este SCRIPT le ayudara a cambiar su nombre de usuario y contrasena en Windows 10! Siga las instrucciones a continuacion:"
 $dispSesionName = "-> Su nombre de usuario actual es: $env:UserName"
 $newUserMsg = "Ingrese el nuevo nombre de usuario para su equipo: "
 
@@ -15,22 +11,25 @@ $newUserNameConf = "n"
 While($newUserNameConf -ne "y"){
     Write-Host $dispSesionName
     $usuario = Read-Host $newUserMsg
-    $newUserNameConf = Read-Host "El nuevo nombre de usuario será: $usuario < Confirma el nuevo nombre de usuario [y/n]? >"
+    $newUserNameConf = Read-Host "El nuevo nombre de usuario sera: $usuario < Confirma el nuevo nombre de usuario [y/n]? >"
 }
-#Cambiar forma en que se consigue el nombre de usuaro actual
 Rename-LocalUser -Name $env:UserName -NewName $usuario
-#Completar
+
 Write-Host "El nombre de usuario se ha actualizado"
 #completar con el nombre de usuario actualizado
 
-#Sistema para determinar si usuario quiere contraseña o no
 #Codigo para cambiar contraseña:
 $condition = 1 #para el While
 While($condition){
   $password = Read-Host "Ingrese nueva contraseña"
     if($password -eq (Read-Host "Re-ingrese contraseña")){
     $condition = 0
-    $password = ConvertTo-SecureString $password -AsPlainText -Force
+    If($password.length -ne 0){
+        $password = ConvertTo-SecureString $password -AsPlainText -Force
+    }
+    Else{
+        $password = ''
+    }
     $UserAccount = Get-LocalUser -Name $usuario
     $UserAccount | Set-LocalUser -Password $password
     Write-Host "Se ha modificado la contraseña"
